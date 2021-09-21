a2406a7a34e7879b37098e2de8eb86cd64b60b58
Brian Ketelsen <bketelsen@gmail.com>
2021-09-21 06:19:42 -0400

    fix broken variables

--- src/initialize.sh
+++ src/initialize.sh
@@ -1,15 +1,15 @@
 XDG_CONFIG_HOME=($HOME/.config)
-dlx_CONFIG_HOME=($XDG_CONFIG_HOME/dlx)
-RECIPE_CONFIG_HOME=($dlx_CONFIG_HOME/recipes)
-PROFILE_CONFIG_HOME=($dlx_CONFIG_HOME/profiles)
-SCRIPT_CONFIG_HOME=($dlx_CONFIG_HOME/scripts)
+DLX_CONFIG_HOME=($XDG_CONFIG_HOME/dlx)
+RECIPE_CONFIG_HOME=($DLX_CONFIG_HOME/recipes)
+PROFILE_CONFIG_HOME=($DLX_CONFIG_HOME/profiles)
+SCRIPT_CONFIG_HOME=($DLX_CONFIG_HOME/scripts)
 
-if [ ! -d "$dlx_CONFIG_HOME" ]; then
-	`mkdir -p $dlx_CONFIG_HOME`;
+if [ ! -d "$DLX_CONFIG_HOME" ]; then
+	`mkdir -p $DLX_CONFIG_HOME`;
 	`mkdir -p $RECIPE_CONFIG_HOME`;
 	`mkdir -p $PROFILE_CONFIG_HOME`;
 	`mkdir -p $SCRIPT_CONFIG_HOME`;
 	create_base;
 	create_scripts;
 fi
-CONFIG_FILE=${dlx_CONFIG_HOME}/dlx.ini
+CONFIG_FILE=${DLX_CONFIG_HOME}/dlx.ini
